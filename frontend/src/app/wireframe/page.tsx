348dce72102e77a4dc7962f9e4658d60613b2819
jhon829 <qwert884@naver.com>
2026-09-10 00:39:21 +0900

    feat(wireframe): connect mock scenarios and page entry points

---
+++ frontend/src/app/wireframe/page.tsx
@@ -0,0 +1,9 @@
+import { AppShell } from "@/components/layout/AppShell";
+import { Wireframe } from "@/mocks/Wireframe";
+export default function Page() {
+  return (
+    <AppShell>
+      <Wireframe />
+    </AppShell>
+  );
+}
